58c20d09a14406de844d65da45fe645802c47a24
Jonathan Elliott <jonathan.elliott1247@gmail.com>
2018-06-10 12:03:57 -0700

    Working on making nav bar responsive.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import * as Semantic from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import NavBar from './NavBar';
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import {Menu, Button, Icon} from 'semantic-ui-react';
 
-const NavBar = () => {
+class NavBar extends React.Component {
 
-    return (
+    constructor(props) {
+        super(props);
+        this.state = {width: document.documentElement.clientWidth};
+        this.updateWidth = this.updateWidth.bind(this);
+    }
+
+    updateWidth() {
+        const width = document.documentElement.clientWidth;
+        this.setState({width});
+    }
+
+    componentDidMount() {
+        this.updateWidth();
+        window.addEventListener('resize', this.updateWidth);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWidth);
+    }
+
+    render() {
+        const {width} = this.state;
+        const location = (
+            <Menu.Item>
+                <Icon size='large' name='location arrow' color='blue'></Icon>
+                <span>Location</span>
+            </Menu.Item>
+        );
+
+        const desktopRight = (
+            <Menu.Menu position='right'>
+                <Menu.Item>
+                    <Icon size='large' name='call square'/>
+                    <span className='call-slogan'>(555) 555-5555</span>
+                </Menu.Item>
+                <Menu.Item>
+                    <Button primary size='mini'>Sign in to Order</Button>
+                </Menu.Item>
+                <Menu.Item>
+                    <Icon size='large' name='shopping cart' color='grey'></Icon>
+                </Menu.Item>
+            </Menu.Menu>
+        );
+
+        const mobileRight = (
+                <Menu.Menu position='right'>
+                <Menu.Item>
+                    <a href='tel:555-555-5555'>
+                        <Icon size='large' name='call square'/>
+                    </a>
+                </Menu.Item>
+                <Menu.Item>
+                    <Button primary size='mini'>Sign in to Order</Button>
+                </Menu.Item>
+                <Menu.Item>
+                    <Icon size='large' name='shopping cart' color='grey'></Icon>
+                </Menu.Item>
+            </Menu.Menu>
+        );
+
+        return (
                 
-                <Menu className='nav'>
+            <Menu className='nav'>
                 <Menu.Item header>
+
                     <Icon size='large' name='chess queen' color='yellow'></Icon>
                     <span>Golden Crown</span>
                 </Menu.Item>
@@ -14,25 +75,13 @@ const NavBar = () => {
                     <Icon size='large' name='food' color='green'></Icon>
                     <span>Order Online</span>
                 </Menu.Item>
-                <Menu.Item>
-                    <Icon size='large' name='location arrow' color='blue'></Icon>
-                    <span>Location</span>
-                </Menu.Item>
-                
-                <Menu.Menu position='right'>
-                    <Menu.Item>
-                        <Icon size='large' name='call square'/>
-                        <span className='call-slogan'>(555) 555-5555</span>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Button primary size='mini'>Sign in to Order</Button>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Icon size='large' name='shopping cart' color='grey'></Icon>
-                    </Menu.Item>
-                </Menu.Menu>
+                {width > 839 ? location :''}
+                {width > 839 ? desktopRight : mobileRight}
+
             </Menu>
-    )
+        )
+    }
+    
 
 }
 
